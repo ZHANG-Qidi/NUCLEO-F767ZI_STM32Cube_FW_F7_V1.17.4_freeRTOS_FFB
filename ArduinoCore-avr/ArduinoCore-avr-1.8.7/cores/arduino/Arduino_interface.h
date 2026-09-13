7a0898ce694ab7df577c1cc385998ed9fa3bb8fe
ZHANG-Qidi <grunt4991@gmail.com>
2026-09-13 14:29:30 +0900

    将组件设置移回组件

--- ArduinoCore-avr/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_interface.h
+++ ArduinoCore-avr/ArduinoCore-avr-1.8.7/cores/arduino/Arduino_interface.h
@@ -21,6 +21,8 @@ void arduino_spi_init(SPI_HandleTypeDef *hspi);
 void arduino_tim_init(TIM_HandleTypeDef *htim, uint64_t TIM_CHANNEL_U, uint64_t TIM_CHANNEL_V, uint64_t TIM_CHANNEL_W);
 void arduino_hi2c_init(I2C_HandleTypeDef *hi2c);
 
+#define SPI_MASTER_CS_IO (PIN("D", 14))
+
 #ifdef __cplusplus
 }
 #endif
